59b091fba1bb7617478ec2a3063534043f3f717b
BrithanMP <brithanmurillo15@gmail.com>
2026-06-30 10:07:46 -0600

    Unidades de medida

--- app/src/main/java/com/my/first/app/MainActivity.kt
+++ app/src/main/java/com/my/first/app/MainActivity.kt
@@ -18,3 +18,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+// El package name se ve como un dominio.
+// Los lenguajes que se suelen usar son kotlin y Java.
+// Se ouede ver cuales sn las plataformas de android mas utilzadas para saber que api usar.
+// Despues de crear el proyecto a la izquierda se ve el explorador de proyectos,
+// a la derecha donde se trabajan  los archivos y abajo el panel de mensajes.
+// Apenas se crea el proyecto da un archivo xml y kt los cuales al correros mostraran un "Hola mundo".
+// Partes de un proyecto base: en la parte superior izquierda donde dice project se puede cambiar la vista, ayudara a ver de otra manera los archivos.
+// En el App esta el proyecto como tal (archivos que conforman el proyecto).
+// En el directorio de JAva se puede ver el archivo que se genero.
+// Dentro de res estara drawable: para meter imagenes (los recursos). layout: las interfaces. Mipmap: las imagenes pero normalmente el icono. values: donde normalmente se guardaran distintos valores (colors, strings y styles).
+// Manifest se encuentra dentro de App, es importante porque es casi la columna del proyecto, dentro de este podemos ver el package, la configuracion en Application.
+// Para provar la app se usa un emulador. Este se puede ver en el AVD Manager, dentro de este se muestran los emuladores definidos por defecto pero tambien se puede elejir otros.
+// Activity: es un consepto (2 archivos), son la base para trabajar una aplicasion.
+// Fragmenst: es una porción de la interfas, es para cambiar una pequeña parte y no all. Se sigue utilizando porque nos permite cambiar las interfaces, se suele ver más en el pastado visual.
+// Gradle: en si es una tecnologia, es un sistema que nos permite trabajar en archivos que nos permitiran configurar la compilasion que queremos. Antes del resultado.
+// Hay 2 archivos llamados gradel script (perspectiva android), el primero hace referencia al proyecto y el segundo hace referencia al modulo.
+// Drawable solo mantendran una version de la imagen mientras que mipmap no importa cuantas versiones tenga, este las mantendra por eso dejan los iconos ahi.
